96112ebe63d22e5b7b34f87329879b7e076636d6
landbased <>
2023-11-07 19:42:11 +1100

    remove dist

--- dist/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNodeError = void 0;
-function isNodeError(error) { return error instanceof Error; }
-exports.isNodeError = isNodeError;
